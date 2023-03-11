88da6e584a48e93822e90d8e7c22be7d3b19222f
sanjaydeveloper15 <sanjaykumarwebs@gmail.com>
2023-03-11 20:47:31 +0530

    binary search program added

---
+++ Searching-Algos/binary-search.js
@@ -0,0 +1,18 @@
+const customInArray = (sortedArray, key) => {
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        if (sortedArray[middle] === key) {
+            return true;
+        } else if (sortedArray[middle] < key) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+	return false;
+}
+
+console.log(customInArray([1,3,5,6,9,14,29,57,89],29));
