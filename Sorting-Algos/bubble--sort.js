a889cbb88a2f2dd8057e19b9efa4753415d8e978
sanjaydeveloper15 <sanjaykumarwebs@gmail.com>
2023-03-11 20:24:01 +0530

    added sorting algorithms

---
+++ Sorting-Algos/bubble--sort.js
@@ -0,0 +1,19 @@
+let unSortArr = [4,-1,34,09,-9,103]
+
+const sortArr = (inputArr) => {
+    for(let i=0; i<inputArr.length; i++)
+    {
+        for(let j=i+1; j<inputArr.length; j++)
+        {
+            let temp = inputArr[i];
+            if(inputArr[i] > inputArr[j])
+            {
+                inputArr[i] = inputArr[j];
+                inputArr[j] = temp;
+            }
+        }
+    }
+    return inputArr;
+}
+
+console.log(sortArr(unSortArr));
