6af1c7ddb1b16c583698aa15978cb78d322a52fe
sanjaydeveloper15 <sanjaykumarwebs@gmail.com>
2023-03-25 20:22:25 +0530

    factorial program added
    Write a program to prints factorial of any number ?

---
+++ Arrays-Questions/factorial-of-num.js
@@ -0,0 +1,10 @@
+// Write a program to prints factorial of any number ?
+const getFactorial = (inputNum) => {
+    let result = 1;
+    for(let i=1; i<=inputNum; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+console.log(getFactorial(5));
