55dfe0d1c763bdbb0e03ad9df3197aced413225c
sanjaydeveloper15 <sanjaykumarwebs@gmail.com>
2023-04-07 20:24:42 +0530

    new programs added
    flatten array program added and working in add key value inside nested object program.

---
+++ Object-Questions/object-add-key.js
@@ -0,0 +1,52 @@
+//Write a program to Add id key with value 1 in each nested object
+const obj = {
+    name: {
+        final: {
+            val: 'yes'
+        }
+    }
+}
+// const outObj = {
+//     id: 1,
+//     name: {
+//         id: 1,
+//         final: {
+//             id: 1,
+//             val: 'yes'
+//         }
+//     }
+// }
+
+const addKeyInObj = (obj,objKeyName='') => {
+    if(objKeyName != ''){
+
+    }
+    //console.log(Object.fromEntries(obj));
+    // console.log(Object.keys(resultObj).length)
+    // obj[key] = val;
+    // let keys = Object.keys(obj);
+    // //resultObj[key] = val;
+    // //resultObj[keys[0]] = obj[keys[0]];
+
+    // if(typeof obj[keys[0]] == 'object')
+    // {
+    //     return addKeyInObj(obj[keys[0]],key,val,obj)
+    // }
+
+    // // for(let [k,v] of Object.entries(obj))
+    // // {
+    // //     //resultObj[k][key] = val;
+    // //     resultObj[k] = obj[k];
+    // //     // if(typeof obj[k] == 'object')
+    // //     // {
+    // //     //     return addKeyInObj(obj[k],key,val,resultObj);
+    // //     // }
+    // // }
+    // if(Object.keys(resultObj).length != 0)
+    // {
+    //     obj = Object.create(resultObj);
+    // }
+    // return resultObj;
+}
+
+//console.log(addKeyInObj(obj,'id',1));
