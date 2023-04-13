1914107e09c2f3006130db979023116d1c69a304
sanjaydeveloper15 <sanjaykumarwebs@gmail.com>
2023-04-13 20:54:06 +0530

    nested object program
    inside nested object add new key value in each object

--- Object-Questions/object-add-key.js
+++ Object-Questions/object-add-key.js
@@ -17,35 +17,14 @@ const obj = {
 //     }
 // }
 
-const addKeyInObj = (obj,objKeyName='') => {
-    if(objKeyName != ''){
-    }
-    //console.log(Object.fromEntries(obj));
-    // console.log(Object.keys(resultObj).length)
-    // obj[key] = val;
-    // let keys = Object.keys(obj);
-    // //resultObj[key] = val;
-    // //resultObj[keys[0]] = obj[keys[0]];
-
-    // if(typeof obj[keys[0]] == 'object')
-    // {
-    //     return addKeyInObj(obj[keys[0]],key,val,obj)
-    // }
-
-    // // for(let [k,v] of Object.entries(obj))
-    // // {
-    // //     //resultObj[k][key] = val;
-    // //     resultObj[k] = obj[k];
-    // //     // if(typeof obj[k] == 'object')
-    // //     // {
-    // //     //     return addKeyInObj(obj[k],key,val,resultObj);
-    // //     // }
-    // // }
-    // if(Object.keys(resultObj).length != 0)
-    // {
-    //     obj = Object.create(resultObj);
-    // }
-    // return resultObj;
+const addKeyInObj = (obj, key, value) => {
+    Object.values(obj).forEach(val => {
+        if (val && typeof val === 'object') {
+            addKeyInObj(val, key, value);
+        }
+    });
+    obj[key] = value;
+    return obj;
 }
 
-//console.log(addKeyInObj(obj,'id',1));
+console.log(addKeyInObj(obj, 'id', 1));
